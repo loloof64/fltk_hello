fdd42813c3ecb675fcffeea49efb7c12f10dfbdb
laurent bernabé <laurent.bernabe@gmail.com>
2023-08-13 16:40:24 +0200

    asking the user firstname for the dialog message output

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
 #![windows_subsystem = "windows"]
 
-use fltk::{prelude::*, window::Window, button::Button, group, app, dialog};
+use fltk::{
+    app,
+    button::Button,
+    dialog, group, input,
+    prelude::{WidgetExt, *},
+    window::Window, frame,
+};
 
 fn main() {
     let a = app::App::default();
-    
-    let mut wind = Window::default().with_size(300, 180).center_screen().with_label("Fltk Hello");
-    
-    let flex = group::Flex::default().with_size(300, 180).column().center_of_parent();
+
+    let mut wind = Window::default()
+        .with_size(300, 180)
+        .center_screen()
+        .with_label("Fltk Hello");
+
+    let flex = group::Flex::default()
+        .with_size(300, 180)
+        .column()
+        .center_of_parent();
+    let flex2 = group::Flex::default()
+        .with_size(300, 90)
+        .row()
+        .center_of_parent();
+    let _label = frame::Frame::default().set_label("Your firstname : ");
+    let input = input::Input::default();
+    flex2.end();
     let mut button = Button::default().with_label("Say hello !");
     flex.end();
 
-    button.set_callback(|_btn| {
-        dialog::message_default("Hello, World !");
+    button.set_callback(move |_btn| {
+        let firstname = input.value();
+        dialog::message_default(format!("Hello, {} !", firstname).as_str());
     });
-    
+
     wind.end();
     wind.show();
 
