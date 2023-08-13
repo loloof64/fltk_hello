bb3412b632f06e39df1ce198e6084dd5fab9338e
laurent bernabé <laurent.bernabe@gmail.com>
2023-08-13 16:24:20 +0200

    showing window with a button

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+#![windows_subsystem = "windows"]
+
+use fltk::{prelude::*, window::Window, button::Button, group, app};
+
 fn main() {
-    println!("Hello, world!");
+    let a = app::App::default();
+    
+    let mut wind = Window::default().with_size(300, 180).center_screen().with_label("Fltk Hello");
+    
+    let flex = group::Flex::default().with_size(300, 180).column().center_of_parent();
+    let mut button = Button::default().with_label("Say hello !");
+    flex.end();
+
+    button.set_callback(|_btn| {
+        println!("Hello World !");
+    });
+    
+    wind.end();
+    wind.show();
+
+    a.run().unwrap();
 }
